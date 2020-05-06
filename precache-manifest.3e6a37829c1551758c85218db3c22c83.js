f246bc12148daf2a36eda9748b8bd7620e3b3fce
afresquet <alvarofresquet@gmail.com>
2020-05-06 18:37:51 +0200

    Updates

---
+++ precache-manifest.3e6a37829c1551758c85218db3c22c83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b287f7f99857af15c11819e515c6ad03",
+    "url": "/text-2-hex/index.html"
+  },
+  {
+    "revision": "f421611f108b5e696fc0",
+    "url": "/text-2-hex/static/js/2.68b2773b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/text-2-hex/static/js/2.68b2773b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c34a472db5682efaabf",
+    "url": "/text-2-hex/static/js/main.b1505df6.chunk.js"
+  },
+  {
+    "revision": "a3a62bab246595a8aee4",
+    "url": "/text-2-hex/static/js/runtime-main.c34ab3c6.js"
+  }
+]);
